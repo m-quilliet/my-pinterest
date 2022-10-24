9e4ac0ca98b896e3f17f81f494a40848e57ed00b
maibelline <mayb80@hotmail.fr>
2022-10-24 17:24:11 +0200

    Add WichUploaderBundle and edit config

--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -6,11 +6,14 @@ use Doctrine\ORM\Mapping as ORM;
 use App\Repository\PinRepository;
 use App\Entity\Traits\Timestampable;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 
 
 /**
  * @ORM\Entity(repositoryClass=PinRepository::class)
+ * #[Vich\Uploadable]
  * @ORM\HasLifecycleCallbacks
  */
 class Pin
@@ -37,6 +40,15 @@ class Pin
      * @Assert\Length(min=10, minMessage="Doit contenir minimum 10 caractéres"))
      */
     private $description;
+    /**
+     * NOTE: This is not a mapped field of entity metadata, just a simple property.
+     *
+     * @Vich\UploadableField(mapping="pin_image", fileNameProperty="imageName")
+     *
+     * @var File|null
+     */
+    private $imageFile;
+
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
@@ -73,6 +85,24 @@ class Pin
         return $this;
     }
 
+    /**
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
+     */
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->setUpdatedAt(new \DateTimeImmutable);
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
     public function getImageName(): ?string
     {
         return $this->imageName;
