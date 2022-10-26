a6921fdb101d1f28bbd41fe649832eaa3e52e678
maibelline <mayb80@hotmail.fr>
2022-10-26 14:03:54 +0200

    Refactoring of SCSS code and better name for pluralize twig extension method

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -3,25 +3,25 @@
 namespace App\Twig;
 
 use Twig\Extension\AbstractExtension;
-use Twig\TwigFilter;
+// use Twig\TwigFilter;
 use Twig\TwigFunction;
 
 class AppExtension extends AbstractExtension
 {
-    public function getFilters(): array
-    {
-        return [
-            // If your filter generates SAFE HTML, you should add a third
-            // parameter: ['is_safe' => ['html']]
-            // Reference: https://twig.symfony.com/doc/3.x/advanced.html#automatic-escaping
-            new TwigFilter('filter_name', [$this, 'doSomething']),
-        ];
-    }
+    // public function getFilters(): array
+    // {
+    //     return [
+    //         // If your filter generates SAFE HTML, you should add a third
+    //         // parameter: ['is_safe' => ['html']]
+    //         // Reference: https://twig.symfony.com/doc/3.x/advanced.html#automatic-escaping
+    //         new TwigFilter('filter_name', [$this, 'doSomething']),
+    //     ];
+    // }
 
     public function getFunctions(): array
     {
         return [
-            new TwigFunction('pluralize', [$this, 'doSomething']),
+            new TwigFunction('pluralize', [$this, 'pluralize']),
         ];
     }
 
