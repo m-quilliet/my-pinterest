8ec48136d01a4c69f3d0591d7561e395349d3959
maibelline <mayb80@hotmail.fr>
2022-11-01 11:40:27 +0100

    Add registration

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -2,21 +2,13 @@
 
 namespace App\Twig;
 
-use Twig\Extension\AbstractExtension;
-// use Twig\TwigFilter;
 use Twig\TwigFunction;
+use Twig\Extension\AbstractExtension;
+
+
 
 class AppExtension extends AbstractExtension
 {
-    // public function getFilters(): array
-    // {
-    //     return [
-    //         // If your filter generates SAFE HTML, you should add a third
-    //         // parameter: ['is_safe' => ['html']]
-    //         // Reference: https://twig.symfony.com/doc/3.x/advanced.html#automatic-escaping
-    //         new TwigFilter('filter_name', [$this, 'doSomething']),
-    //     ];
-    // }
 
     public function getFunctions(): array
     {
